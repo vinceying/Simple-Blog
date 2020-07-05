3fbeb6973b53d90d9c4235ed174d7e8b651d890e
vinceying <admin@vince.pub>
2020-07-05 22:17:20 +0800

    Update Blog

---
+++ blog.config.js
@@ -0,0 +1,29 @@
+const BLOG = {
+    author: 'Vince',
+    title: 'Vince',
+    description: "前端学习中，希望可以多掌握一些技术栈",
+    summary: '前端学习中，希望可以多掌握一些技术栈',
+    language: 'zh-cn',
+    latestLimit: 5,
+
+    labels: {
+        default: '所有文章',
+        latest: '最近',
+        list: '所有',
+    },
+
+    email: 'admin@vince.pub',
+    github: 'vinceying',
+    domain: 'i.vince.pub',
+
+    googleAnalytics: '',
+    cn: true,
+    enableViews: false,
+
+    layouts: {
+        pageWidth: '750px',
+        pageWidthMobile: '88vw',
+    },
+}
+
+export default BLOG
